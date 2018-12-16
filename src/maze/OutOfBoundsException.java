e66bacbaed9a1031051e03d48bc0b08debebdb9f
js42721 <js42721@gmail.com>
2018-12-15 23:29:23 -0500

    Edited

--- src/maze/OutOfBoundsException.java
+++ src/maze/OutOfBoundsException.java
@@ -3,9 +3,7 @@ package maze;
 import java.io.Serializable;
 
 /** Indicates that coordinates are out of range. */
-public class OutOfBoundsException extends RuntimeException
-        implements Serializable {
-
+public class OutOfBoundsException extends RuntimeException implements Serializable {
     private static final long serialVersionUID = -9142449090078209915L;
 
     /** Constructs an {@code OutOfBoundsException}. */
